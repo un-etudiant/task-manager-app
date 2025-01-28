4fba74a1165b61e1f28184a4314fe06ecae03b83
Pradeep Sundaram <pradeepssundaram@outlook.com>
2025-01-28 23:53:07 +0530

    Completed Backend Service Changes

--- backend/task-manager-service/src/main/java/com/taskmanager/task_manager_service/config/DynamoDbConfig.java
+++ backend/task-manager-service/src/main/java/com/taskmanager/task_manager_service/config/DynamoDbConfig.java
@@ -1,4 +1,4 @@
-package com.taskmanager.config;
+package com.taskmanager.task_manager_service.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
